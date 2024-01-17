b4b81be8fe59c91d4035e157b2b7e38902e33507
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-01-17 20:15:30 +0300

    Modify password reset endpoint

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,20 +1,26 @@
-from dj_rest_auth.serializers import PasswordResetSerializer, LoginSerializer, PasswordResetConfirmSerializer
-from rest_framework import serializers
+from dj_rest_auth.serializers import (
+    LoginSerializer,
+    PasswordResetConfirmSerializer,
+    PasswordResetSerializer,
+)
 from django.conf import settings
+from rest_framework import serializers
 
-RESET_PASSWORD_REDIRECT_URL = settings.FRONT_END_URLS.get("PASSWORD_RESET") 
+RESET_PASSWORD_REDIRECT_URL = settings.FRONT_END_URLS.get("PASSWORD_RESET")
+
 
 def custom_url_generator(request, user, temp_key):
-    return f'{RESET_PASSWORD_REDIRECT_URL}/{user.id}/{temp_key}'
+    return f"{RESET_PASSWORD_REDIRECT_URL}/{user.id}/{temp_key}"
+
+
 class MyLoginSerializer(LoginSerializer):
     username = None
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
-class MyPasswordResetSerializer(PasswordResetSerializer):
 
-    def get_email_options(self) :
+class MyPasswordResetSerializer(PasswordResetSerializer):
+    def get_email_options(self):
         return {
-            'url_generator': custom_url_generator,
+            "url_generator": custom_url_generator,
         }
-    
