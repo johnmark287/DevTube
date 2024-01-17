f66261e74173b6f7c96b16bad7371e98af9ea2a5
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-01-17 20:30:40 +0300

    Document serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,5 @@
-from dj_rest_auth.serializers import (
-    LoginSerializer,
-    PasswordResetConfirmSerializer,
-    PasswordResetSerializer,
-)
+"""Serializers for accounts app."""
+from dj_rest_auth.serializers import LoginSerializer, PasswordResetSerializer
 from django.conf import settings
 from rest_framework import serializers
 
@@ -10,17 +7,23 @@ RESET_PASSWORD_REDIRECT_URL = settings.FRONT_END_URLS.get("PASSWORD_RESET")
 
 
 def custom_url_generator(request, user, temp_key):
+    """Custom url generator for password reset."""
     return f"{RESET_PASSWORD_REDIRECT_URL}/{user.id}/{temp_key}"
 
 
 class MyLoginSerializer(LoginSerializer):
+    """Custom login serializer."""
+
     username = None
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
 
 class MyPasswordResetSerializer(PasswordResetSerializer):
+    """Custom password reset serializer."""
+
     def get_email_options(self):
+        """Custom email options."""
         return {
             "url_generator": custom_url_generator,
         }
