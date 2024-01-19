35d0cdbc96caf9e55e6f801c467eaefefdb651b4
Karume-lab <danielkarume@outlook.com>
2024-01-19 12:14:32 +0300

    configured google authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
+    path("google/", views.GoogleLogin.as_view(), name="google_login"),
 ]
